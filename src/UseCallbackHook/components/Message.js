62265debca2de4a37b9f4ec4aaf12bac38d96955
Rakibul-Hasan <rakib.it.ergon@gmail.com>
2025-07-16 12:42:28 +0600

    React Hooks | useMemo Hook - Rakib office

--- src/UseCallbackHook/components/Message.js
+++ src/UseCallbackHook/components/Message.js
@@ -1,10 +1,22 @@
-import React, {memo} from 'react'
+import React, {memo, useMemo} from 'react'
 
 
 const Message = ({numberOfMessages, onHandleIncrement}) => {
     console.log("Message rendering")
+
+  const calculatedNumber = useMemo(
+          ()=>{
+              let number = 0;
+              for (let index = 0; index < 5000000000; index++) {
+              number ++;
+            
+          }
+          return number
+        }, [])
+
   return (
     <div>
+        <h2>Number: {calculatedNumber}</h2>
         <p>send {numberOfMessages} Message</p>
         <div><br/>
           <button onClick={onHandleIncrement}>Increment Message Number</button>
